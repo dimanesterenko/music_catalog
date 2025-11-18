4e283bfc9d01032fc876a99d7f36bd3744d675d8
dimanesterenko <nesterenkoodmytro@gmail.com>
2025-11-18 12:26:24 +0200

    modified:   backend/__pycache__/main.cpython-312.pyc 	modified:   backend/main.py 	modified:   backend/services/__pycache__/music.cpython-312.pyc 	modified:   backend/services/music.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from services.music import search_artist
+from services.music import search_artists,get_artist_releases
 app = FastAPI()
 
 @app.get('/api/artists')
 async def search_artist_by_name(q:str):
-    return await search_artist(q)
+    return await search_artists(q)
+
+@app.get('/api/artist/{artist_id}/releases')
+async def releases(artist_id:str):
+    return await get_artist_releases(artist_id)
--- backend/services/music.py
+++ backend/services/music.py
@@ -8,24 +8,26 @@ HEADERS = {
 }
 
 
-async def search_artist(query:str):
+async def search_artists(query:str):
     async with httpx.AsyncClient() as client:
         response = await client.get(
             f"{MUSICBRAINZ_URL}/artist",
             params={"query": query, "fmt": "json"},
             headers=HEADERS
         )
-        print("STATUS:", response.status_code)
-        print("BODY:", response.text)
+        # print("STATUS:", response.status_code)
+        # print("BODY:", response.text)
         return response.json()
     
-
-async def get_artist_releases(artist_id:int):
+async def get_artist_releases(artist_mbid: str):
     async with httpx.AsyncClient() as client:
         response = await client.get(
-            f"{MUSICBRAINZ_URL}/artist",
-            params={"artist_id": artist_id, "fmt": "json"},
+            f"{MUSICBRAINZ_URL}/release/",
+            params={
+                "artist": artist_mbid,   # MBID артиста
+                "fmt": "json",
+                "limit": 100
+            },
             headers=HEADERS
         )
-        
         return response.json()
